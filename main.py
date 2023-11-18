4cfe5ae01d895811f5af3dfb9f71b7833f8d6a2f
Juan G. Saurith M <juanguisam@gmail.com>
2023-11-18 17:13:53 -0500

    THE MAIN PYTHON FILE IS CREATED 🐍

---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+Bienvenidos al proyecto OhMyTask, este es el archivo fuente apartir del cual se ejecuta el programa.
+Este proyecto viene a ser una propuesta de software para la gestión efectiva de tareas.
+Programador princial: Juan Guillermo Saurith Moreno
+Asignatura: Programación I
+"""
+
+# funciones a utilizar
+
+
+def crearTarea():
+    return 0
+
+
+def eliminarTarea():
+    return 0
+
+
+def modificarTarea():
+    return 0
+
+
+def realizarTarea():
+    return 0
