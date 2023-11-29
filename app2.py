674eb20e4be7808a609b6fdc30bd40700da97af0
Juan G. Saurith M. <juanguisam@gmail.com>
2023-11-28 22:43:26 -0500

    WE'RE MAKING THE INTERFACE 2.0 ❤️

--- app2.py
+++ app2.py
@@ -1,15 +1,19 @@
 import tkinter as tk
 from tkinter import simpledialog
+from tkinter import ttk
 
 class OhMyTasks():
+    """
+    Clase principal de la aplicación
+    """
     def __init__ (self, root): # El argumento self funciona como un constructor
         # -----------------------------------------------------------------------
         # Defino nuestro root (Programa principal)
         # Atributos principales
         self.root = root
-        self.root.title = "OhMyTasks" # Titulo de la ventana
+        self.root.title("OhMyTasks") # Titulo de la ventana
         self.root.resizable(0, 0)     # Tamaño estático
-
+        self.root.configure(background="#EEF5FF") # Color de fondo
         # Variables bandera
         self.listaSeleccionada_listbox = tk.StringVar()
         self.listaSeleccionada_otros = tk.StringVar()
@@ -18,13 +22,16 @@ class OhMyTasks():
         self.listaSeleccionada_listbox.set("Tareas Pendientes")
         self.listaSeleccionada_otros.set("Tareas Pendientes") # Constante
         # -----------------------------------------------------------------------
-
         # DISEÑO INTERFAZ
+
+        style = ttk.Style()
+        style.configure("TButton", background="#EEF5FF")
+
         # Botones
-        self.botonCrear = tk.Button(root, text="Crear", comand="")
-        self.botonEditar = tk.Button(root, text="Crear", comand="")
-        self.botonBorrar = tk.Button(root, text="Crear", comand="")
-        self.botonSwitch = tk.Button(root, text="Crear", comand="")
+        self.botonCrear = ttk.Button(root, text="Crear", command=self.CrearTarea)
+        self.botonEditar = tk.Button(root, text="Modificar", command=self.EditarTarea)
+        self.botonBorrar = tk.Button(root, text="Eliminar", command=self.BorrarTarea)
+        self.botonSwitch = tk.Button(root, text="Cambiar Lista", command=self.CambioLista)
 
         # Listbox (Listas)
         # Titulos
@@ -33,7 +40,7 @@ class OhMyTasks():
         # Listbox
         self.tareasPendientes = tk.Listbox(root, selectmode=tk.SINGLE)
         self.tareasRealizadas = tk.Listbox(root, selectmode=tk.SINGLE)
-        
+
         #------------------------------------------------------------------------
 
         # POSICIONAMIENTO
@@ -53,6 +60,59 @@ class OhMyTasks():
         self.botonSwitch.grid(row=2, column=1)
         self.tareasRealizadas.grid(row=2, column=2)
 
+    #--------------------------------------------------------------------------------
+    # MÉTODOS
+    def CambioLista(self):
+        listaSeleccionadaActual = self.listaSeleccionada_listbox.get()
+        if listaSeleccionadaActual == "Tareas Pendientes":
+            indice = self.tareasPendientes.curselection()
+            if indice:
+                tituloTarea = self.tareasPendientes.get(indice)
+                self.tareasPendientes.delete(indice)
+                self.tareasRealizadas.insert(tk.END, tituloTarea)
+        else:
+            indice = self.tareasRealizadas.curselection()
+            if indice:
+                tituloTarea = self.tareasRealizadas.get(indice)
+                self.tareasRealizadas.delete(indice)
+                self.tareasPendientes.insert(tk.END, tituloTarea)
+
+        self.listaSeleccionada_listbox.set("Tareas Pendientes" if listaSeleccionadaActual == "Tareas Realizadas" else "Tareas Realizadas")
+
+    def CrearTarea(self):
+        titulo = simpledialog.askstring("Crear Tarea", "Ingrese el título de la tarea")
+        if titulo:
+            listaSeleccionadaActual = self.listaSeleccionada_otros.get()
+            if listaSeleccionadaActual == "Tareas Pendientes":
+                self.tareasPendientes.insert(tk.END, titulo)
+
+    def EditarTarea(self):
+        listaSeleccionadaActual = self.listaSeleccionada_otros.get()
+        if listaSeleccionadaActual == "Tareas Pendientes":
+            indice = self.tareasPendientes.curselection()
+            titulo = self.tareasPendientes.get(indice)
+            nuevoTitulo = simpledialog.askstring("Editar Tarea", "Ingrese el nuevo título de la tarea", initialvalue=titulo)
+
+            if titulo and nuevoTitulo:
+                self.tareasPendientes.delete(indice)
+                self.tareasPendientes.insert(indice, nuevoTitulo)
+
+    def BorrarTarea(self):
+        listaSeleccionadaActual = self.listaSeleccionada_otros.get()
+        if listaSeleccionadaActual == "Tareas Pendientes":
+            indice = self.tareasPendientes.curselection()
+            self.tareasPendientes.delete(indice)
+
+
+
 def main():
-    
+    root = tk.Tk()
+    app = OhMyTasks(root)
+    root.mainloop()
+
+main()
+
+
+
+
 
--- interfaz.py
+++ interfaz.py
@@ -1,9 +1,105 @@
-# from main import *
 import tkinter as tk
 from tkinter import simpledialog
 
-# Ventana principal
-window = tk.Tk()
-window.title("OhMyTask")
-window.geometry("400x500")
-window.resizable(False, False)
+class OhMyTasks:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("OhMyTask")
+
+        # Variables
+        self.current_switch_listbox = tk.StringVar()
+        self.current_listbox_operations = tk.StringVar()
+        self.current_switch_listbox.set("Tareas Pendientes")
+        self.current_listbox_operations.set("Tareas Pendientes")
+
+        # Botones superiores
+        self.create_button = tk.Button(root, text="Crear", command=self.create_task)
+        self.delete_button = tk.Button(root, text="Eliminar", command=self.delete_task)
+        self.modify_button = tk.Button(root, text="Modificar", command=self.modify_task)
+        self.switch_button = tk.Button(root, text="Cambiar Lista", command=self.switch_listbox)
+
+        # Listas
+        self.pending_tasks_label = tk.Label(root, text="Tareas Pendientes")
+        self.completed_tasks_label = tk.Label(root, text="Tareas Realizadas")
+
+        self.pending_tasks_listbox = tk.Listbox(root, selectmode=tk.SINGLE)
+        self.completed_tasks_listbox = tk.Listbox(root, selectmode=tk.SINGLE)
+
+        # Posicionar elementos
+        self.create_button.grid(row=0, column=0)
+        self.delete_button.grid(row=0, column=1)
+        self.modify_button.grid(row=0, column=2)
+        self.switch_button.grid(row=2, column=1)
+
+        self.pending_tasks_label.grid(row=1, column=0)
+        self.completed_tasks_label.grid(row=1, column=2)
+
+        self.pending_tasks_listbox.grid(row=2, column=0)
+        self.completed_tasks_listbox.grid(row=2, column=2)
+
+    def switch_listbox(self):
+        current_switch_listbox = self.current_switch_listbox.get()
+        current_listbox_operations = self.current_listbox_operations.get()
+
+        if current_switch_listbox == "Tareas Pendientes":
+            selected_index = self.pending_tasks_listbox.curselection()
+            if selected_index:
+                task_title = self.pending_tasks_listbox.get(selected_index)
+                self.pending_tasks_listbox.delete(selected_index)
+                self.completed_tasks_listbox.insert(tk.END, task_title)
+        else:
+            selected_index = self.completed_tasks_listbox.curselection()
+            if selected_index:
+                task_title = self.completed_tasks_listbox.get(selected_index)
+                self.completed_tasks_listbox.delete(selected_index)
+                self.pending_tasks_listbox.insert(tk.END, task_title)
+
+        self.current_switch_listbox.set("Tareas Pendientes" if current_switch_listbox == "Tareas Realizadas" else "Tareas Realizadas")
+
+    def create_task(self):
+        title = simpledialog.askstring("Crear Tarea", "Ingrese el título de la tarea:")
+
+        if title:
+            current_listbox_operations = self.current_listbox_operations.get()
+
+            if current_listbox_operations == "Tareas Pendientes":
+                self.pending_tasks_listbox.insert(tk.END, title)
+            else:
+                self.completed_tasks_listbox.insert(tk.END, title)
+
+    def delete_task(self):
+        current_listbox_operations = self.current_listbox_operations.get()
+        selected_index = None
+
+        if current_listbox_operations == "Tareas Pendientes":
+            selected_index = self.pending_tasks_listbox.curselection()
+            self.pending_tasks_listbox.delete(selected_index)
+        elif current_listbox_operations == "Tareas Realizadas":
+            selected_index = self.completed_tasks_listbox.curselection()
+            self.completed_tasks_listbox.delete(selected_index)
+
+    def modify_task(self):
+        current_listbox_operations = self.current_listbox_operations.get()
+
+        if current_listbox_operations == "Tareas Pendientes":
+            selected_index = self.pending_tasks_listbox.curselection()
+            title = self.pending_tasks_listbox.get(selected_index)
+        elif current_listbox_operations == "Tareas Realizadas":
+            selected_index = self.completed_tasks_listbox.curselection()
+            title = self.completed_tasks_listbox.get(selected_index)
+
+        new_title = simpledialog.askstring("Modificar Tarea", "Ingrese el nuevo título de la tarea:", initialvalue=title)
+
+        if title and new_title:
+            if current_listbox_operations == "Tareas Pendientes":
+                self.pending_tasks_listbox.delete(selected_index)
+                self.pending_tasks_listbox.insert(tk.END, new_title)
+            elif current_listbox_operations == "Tareas Realizadas":
+                self.completed_tasks_listbox.delete(selected_index)
+                self.completed_tasks_listbox.insert(tk.END, new_title)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = OhMyTasks(root)
+    root.mainloop()
+
